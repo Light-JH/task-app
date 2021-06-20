9be3ada13b9409bccbaab567bc1154831a33ad5c
Light-JH <phoebehu@outlook.com>
2021-06-20 13:30:15 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import styled from 'styled-components'
+import { DragDropContext } from 'react-beautiful-dnd'
+import initialData from './initial-data'
+import Column from './column'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Container = styled.div`
+  display: flex;`
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  state = initialData;
+  onDragStart = start => {
+    const homeIndex = this.state.columnOrder.indexOf(start.source.droppableId)
+
+    this.setState({
+      homeIndex
+    })
+  }
+// clear the index when the drag finish 
+  onDragEnd=result=>{
+    this.setState({
+      homeIndex:null,
+    })
+
+    const {source, destination, draggableId} = result;
+
+    if(!destination) {return}
+    if(
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const start = this.state.columns[source.droppableId]// source, column:id-title-taskIds 
+    const finish = this.state.columns[destination.droppableId]// desitination column
+
+    if (start === finish) {
+      
+      const newTaskIds = Array.from(start.taskIds);
+      console.log(source.droppableId)//column-1
+
+      newTaskIds.splice(source.index,1);
+      newTaskIds.splice(destination.index, 0, draggableId)
+      
+      const newColumn = {
+        ...start,
+        taskIds:newTaskIds
+      }
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]:newColumn
+        }
+      }
+
+      this.setState(newState)
+      return;
+    }
+
+    const startTaskIds = Array.from(start.taskIds);
+    startTaskIds.splice(source.index,1)
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds
+    }
+    const finishTaskIds = Array.from(finish.taskIds)
+    // update the taskIds in source and destination column: add draggableId to the new destination.column 
+     
+    finishTaskIds.splice(destination.index,0,draggableId)
+    const newFinish = {
+      ...finish,
+      taskIds:finishTaskIds,
+    }
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]:newFinish
+      }       
+    }
+    this.setState(newState);
+  }
+
+  render(){
+    return (
+      <DragDropContext 
+        onDragStart={this.onDragStart}
+        onDragEnd={this.onDragEnd}>
+          
+          <Container>
+            {this.state.columnOrder.map((columnId, index) => {
+              const column = this.state.columns[columnId];
+              const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+              return (<Column key={column.id} column={column} tasks={tasks}/>);       
+            })}
+          </Container>
+          
+      </DragDropContext>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+
---
+++ src/task.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Draggable } from 'react-beautiful-dnd'
+
+const Container = styled.div`
+border: 1px solid lightgrey;
+border-radius: 2px;
+padding: 8px;
+margin-bottom: 8px;
+background-color: white;
+background-color: ${props => (
+    props.isDragDisabled ? 'lightgrey' : props.isDragging ? 'lightgreen' : 'white'
+    )}`;
+
+// Draggable's child should be a function ()=>{}
+export default class Task extends React.Component {
+    render(){
+        console.log(this.props.index, this.props.task)
+        const isDragDisabled = this.props.task.id ==='task-1';
+        return (
+            
+            <Draggable 
+                draggableId={this.props.task.id} 
+                index={this.props.index}
+                isDragDisabled={isDragDisabled}
+            >
+                {(provided, snapshot)=>(
+                    <Container
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                        isDragging={snapshot.isDragging}
+                        isDragDisabled={isDragDisabled}
+                        >
+                          {this.props.task.content}
+                    </Container>
+                )}
+            </Draggable>
+            
+        )
+    }
+}
